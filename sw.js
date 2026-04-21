548c422846fc3b17417020565877c6bfed7732cd
enriquin12 <151086854+enriquin12@users.noreply.github.com>
2026-04-20 22:16:46 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'carteira-v1';
+const ASSETS = [
+  'index.html',
+  'pagina2.html',
+  'manifest.json',
+  'frente.jpeg',
+  'verso.jpeg',
+  'qrcode.jpeg',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS.map(a => new Request(a, { cache: 'reload' })))).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => cached))
+  );
+});
